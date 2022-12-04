a1d09173577b6c9122086d6126a9c3be2587502e
rockyz7 <roocky_16@hotmail.com>
2022-12-04 02:20:24 -0300

    Creación de rutas, guardado de API response en contexto global, renderizado de cards en componente Home

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
   return (
-    <nav>
+    <nav style={{width: "90vw", marginLeft: "50px", display: "flex", justifyContent: "space-between"}}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <div>
+      <NavLink to="./home" style={{ fontSize: "1.5rem"}}><span style={{color: "red"}}>D</span>H Odonto</NavLink>
+      </div>
+      <div>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/favs">Favs</NavLink>
+      <button style={{backgroundColor: "black", borderRadius: "7px"}}>🌙</button>
+      </div>
+ 
     </nav>
   )
 }
--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,52 @@
-import { createContext } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import axios from "axios"
 
 export const initialState = {theme: "", data: []}
 
-export const ContextGlobal = createContext(undefined);
+export const ContextGlobal = createContext();
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'FETCH_SUCCESS':
+    return {
+        data: action.payload,
+    }
+
+    case 'FETCH_ERROR':
+    return {
+        data: [],
+    }
+
+    default:
+        return state
+}
+}
 
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        dispatch({type: 'FETCH_SUCCESS', payload: response.data})
+
+    })
+    .catch(error => {
+        dispatch({type: 'FETCH_ERROR'})
+    })
+   }, [])
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={state}>
       {children}
     </ContextGlobal.Provider>
   );
 };
+
+
+
+
+
+
